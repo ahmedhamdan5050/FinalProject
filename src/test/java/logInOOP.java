fb4f8c5b7644dc279afd2f5b01cab7f77f8e30c1
ahmedhamdan5050 <ahmedhamdan5050@gmail.com>
2022-10-06 06:48:08 -0400

    final push

---
+++ src/test/java/logInOOP.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+
+public class logInOOP {
+    public By SignIn=By.cssSelector("div.header_user_info > a");
+    public By email=By.cssSelector("#email");
+    public By password=By.cssSelector("#passwd");
+    public By loginButton=By.xpath("//*[@id=\"SubmitLogin\"]");
+    public By WomenTab=By.cssSelector("#block_top_menu > ul > li:nth-child(1) > a");
+    public By Catagory=By.cssSelector("#ul_layered_category_0 > li:nth-child(1) > label > a");
+    public By Size=By.cssSelector("#ul_layered_id_attribute_group_1 > li:nth-child(1) > label > a");
+    public By SelectItems=By.xpath("//*[@id=\"center_column\"]/ul");
+    public By AddToCartButton=By.xpath("//*[@id=\"add_to_cart\"]/button/span");
+    public By ContinueShoppingButton=By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/span/span");
+    public By QuantityBox=By.cssSelector("span.ajax_cart_quantity");
+    public By Cart=By.xpath("//*[@id=\"header\"]/div[3]/div/div/div[3]/div/a");
+    public By ProccedToCheckoutButton=By.xpath("//*[@id=\"center_column\"]/p[2]/a[1]");
+    public By addressProccedToCheckout=By.cssSelector("#center_column > form > p > button > span");
+    public By termsAndConditon=By.xpath("//*[@id=\"form\"]/div/p[2]/label");
+    public By ShippingProccedToCheckout=By.xpath("//*[@id=\"form\"]/p/button/span");
+}
--- src/test/java/login.java
+++ src/test/java/login.java
@@ -1,2 +1,95 @@
+import Utils.DriverUtils;
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static Utils.DriverUtils.getWebDriver;
+import static Utils.DriverUtils.waitAndClick;
+
 public class login {
+    WebDriver driver;
+logInOOP oop=new logInOOP();
+    @BeforeClass
+    void setup() throws InterruptedException {
+        driver = getWebDriver();
+        driver.get("http://automationpractice.com/");
+        driver.manage().window().fullscreen();
+        Thread.sleep(5000);
+    }
+
+    @Test
+    void logInFromProperties() throws InterruptedException {
+        driver.findElement((oop.SignIn)).click();
+        DriverUtils.waitAndClick(driver,(oop.email), 15);
+        WebElement usernameEl = driver.findElement((oop.email));
+        String email = DriverUtils.initializeProperties().getProperty("email");
+        usernameEl.sendKeys(email);
+        DriverUtils.waitAndClick(driver,(oop.password), 15);
+        WebElement userpassEl = driver.findElement((oop.password));
+        String password = DriverUtils.initializeProperties().getProperty("password");
+        userpassEl.sendKeys(password);
+        DriverUtils.waitAndClick(driver,(oop.loginButton), 15);
+        DriverUtils.waitAndClick(driver, (oop.WomenTab), 15);
+        driver.manage().window().fullscreen();
+        DriverUtils.waitAndClick(driver,(oop.Catagory), 15);
+        DriverUtils.waitAndClick(driver,(oop.Size), 15);
+
+        List<WebElement> el = driver.findElements(oop.SelectItems);
+        Faker f = new Faker();
+        int randomnum = f.random().nextInt(0, el.size() - 1);
+        el.get(randomnum).click();
+        Thread.sleep(5000);
+        DriverUtils.waitAndClick(driver,(oop.AddToCartButton), 15);
+        DriverUtils.waitAndClick(driver, (oop.ContinueShoppingButton), 15);
+        String quantity = driver.findElement(oop.QuantityBox).getText();
+
+        int counter = 1;
+        Assert.assertTrue(quantity.contains( String.valueOf(counter)));
+        waitAndClick(driver,oop.WomenTab,15);
+        DriverUtils.waitAndClick(driver,(oop.Catagory), 15);
+        DriverUtils.waitAndClick(driver,(oop.Size), 15);
+
+        List<WebElement> ele = driver.findElements(oop.SelectItems);
+        Faker faker= new Faker();
+        int randomnum2= faker.random().nextInt(0, ele.size() - 1);
+     ele.get(randomnum2).click();
+        Thread.sleep(5000);
+        waitAndClick(driver,(oop.AddToCartButton),15);
+        waitAndClick(driver,(oop.ContinueShoppingButton),15);
+        Assert.assertTrue(quantity.contains(String.valueOf(counter++)));
+
+       waitAndClick(driver,(oop.Cart),15);
+       waitAndClick(driver,(oop.ProccedToCheckoutButton),15);
+        waitAndClick(driver,(oop.addressProccedToCheckout),15);
+        waitAndClick(driver,(oop.termsAndConditon),15);
+        waitAndClick(driver,(oop.ShippingProccedToCheckout),15);
+        Thread.sleep(5000);
+        String TotalProductPrice=(driver.findElement(By.cssSelector("#total_product"))).getText();
+        TotalProductPrice=TotalProductPrice.replace("$","");
+        String ShippingPrice=driver.findElement(By.cssSelector("#total_shipping")).getText();
+        ShippingPrice=ShippingPrice.replace("$","");
+        String total= ((driver.findElement(By.cssSelector("#total_price_container"))).getText());
+        Double assartPrice= (Double.valueOf(TotalProductPrice) + Double.valueOf(ShippingPrice));
+
+        System.out.println(total);
+        System.out.println(assartPrice);
+        Assert.assertTrue(total.contains(String.valueOf(assartPrice)));
+
+        // write code to add another item
+//        Assert.assertTrue(quantity.contains( String.valueOf(counter++) ));
+//        // write code to add another item
+//        Assert.assertTrue(quantity.contains( String.valueOf(counter++) ));
+    }
+    @AfterClass
+    void quit() {
+        driver.quit();
+    }
 }
+
